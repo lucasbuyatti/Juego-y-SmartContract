8d64068a4fd1af036c8487e480da48b67d62541a
Lucas <lucasbsftwr@gmail.com>
2025-01-20 00:17:23 -0300

    Creacion del juego

---
+++ sui_dapp/src/components/game/game.tsx
@@ -0,0 +1,22 @@
+import { Box, Text } from "@radix-ui/themes";
+
+export function GameTaTeTi() {
+
+    return (
+
+        <Box
+        style={{
+          border: "1px solid red",
+          height: "12vh",
+          width: "calc(100% - 4rem)",
+          padding: "1.2rem",
+          marginTop: "1rem",
+          marginLeft: "2rem",
+          marginRight: "2rem",
+        }}
+      >
+        <Text>Hola</Text>
+      </Box>
+
+    );
+}
