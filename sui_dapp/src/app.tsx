b02eb7a782d8541209a4a28b92ee3809eba272e9
Lucas <lucasbsftwr@gmail.com>
2025-01-20 00:15:25 -0300

    Cambio general

--- sui_dapp/src/app.tsx
+++ sui_dapp/src/app.tsx
@@ -1,10 +1,23 @@
-import { Header } from './components/header/Header'
+import { ConnectButton } from "@mysten/dapp-kit";
+import { Box, Text, Theme } from "@radix-ui/themes";
+import { Header } from "./components/header/Header";
+import { GameTaTeTi } from "./components/game/game";
+
 
 export function App() {
 
   return (
+    <>
+      <Theme 
+      appearance="dark" 
+      accentColor="violet"
+      panelBackground="translucent"
+      >
+        <Header/>
+        <GameTaTeTi/>
 
-    <Header></Header>
-      
+      </Theme>
+    </>
+
   );
 }
